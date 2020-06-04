49e3df0cb24b404e48e1965eb8cab1b621ae1bca
Keesung CHOI <keesung@Keesungui-MacBookPro.local>
2020-06-04 21:11:38 +0900

    7장 컴포넌트 라이프 사이클

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from "react";
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from './Say';
 // import EventPractice from './EventPractice';
-import IterationSample from './IterationSample';
-import './App.css';
+// import IterationSample from './IterationSample';
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+import "./App.css";
 
 // function App() {
 //   const name = '리액트';
@@ -24,14 +26,41 @@ import './App.css';
 //   );
 // }
 
-const App = () => {
-  return (
-    <IterationSample />
-  );
-  // <EventPractice />;
-  // <Say />;
-  // <Counter/>;
-  // <MyComponent name="React" favoriteNumber={2}>리액트</MyComponent>
-};
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <LifeCycleSample />
+//   );
+// <EventPractice />;
+// <Say />;
+// <Counter/>;
+// <MyComponent name="React" favoriteNumber={2}>리액트</MyComponent>
+// };
 
 export default App;
